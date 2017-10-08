01e688ce81cf5345e0d43a33e9019547234322c3
David Reens <dave.reens@gmail.com>
2017-10-07 21:47:47 -0600

    Partial Image for point by point

--- partialImage.m
+++ partialImage.m
@@ -3,6 +3,8 @@
 % the cluster thus far
 function ofli2 = partialImage(N,E,X,trap,P,varargin)
 
+% Two different cases depending on whether the image was run with a version
+% that supported different planes at that time.
 datadir = '//data/ye/dare4983/splines/';
 if isempty(varargin)
     thisdir = sprintf('N%d_E%d_X%d_%s_P%d',N,E,X,trap,P);
@@ -11,20 +13,37 @@ else
 end
 assert(~~exist([datadir thisdir],'dir'),'No partial data found for those parameters');
 
-ofli2 = zeros(3*N,N,20);
-maxi = 1;
-for i=1:3*N
-    dir2file = [datadir thisdir '/i=' num2str(i) '.mat'];
-    if exist(dir2file,'file')
-        tmp = load(dir2file);
-        if N~=size(tmp.stash,1)
-            ofli2 = zeros(3*N,size(tmp.stash,1),20);
+% Two cases depending on whether point by point parfor had been implemented
+% yet or not.
+dimfile = [datadir thisdir '/dims.mat'];
+if exist(dimfile,'file')
+    load(dimfile,'N1','N2');
+    ofli2 = zeros(N1*N2,20);
+    for i=1:N1*N2
+        dir2file = [datadir thisdir '/i=' num2str(i) '.mat'];
+        if exist(dir2file,'file')
+            tmp = load(dir2file);
+            ofli2(i,:) = tmp.stash;
         end
-        ofli2(i,:,:) = tmp.stash;
-        maxi = i;
     end
+    ofli2 = reshape(ofli2,[N1 N2 20]);
+else
+    ofli2 = zeros(3*N,N,20);
+    maxi = 1;
+    for i=1:3*N
+        dir2file = [datadir thisdir '/i=' num2str(i) '.mat'];
+        if exist(dir2file,'file')
+            tmp = load(dir2file);
+            if N~=size(tmp.stash,1)
+                ofli2 = zeros(3*N,size(tmp.stash,1),20);
+            end
+            ofli2(i,:,:) = tmp.stash;
+            maxi = i;
+        end
+    end
+    ofli2 = ofli2(1:maxi,:,:);
 end
-ofli2 = ofli2(1:maxi,:,:);
+
 cut = 10;
 last = broaden(ofli2(:,:,end));
 lost = (last==1);
